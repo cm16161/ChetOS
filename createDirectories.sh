5847ea3fd212efddb716ab4ff0d94e3f493bd786
Chetan Mistry <cmistry98@gmail.com>
2019-09-05 13:30:20 +0100

    Add files and scripts
    This commit adds more scripts and files to save time making them every time

---
+++ createDirectories.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $(whoami) != "lfs" ]] ; then
+    echo "Not running as user lfs, you should be!"
+    exit 1
+fi
+
+mkdir -pv /{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
+mkdir -pv /{media/{floppy,cdrom},sbin,srv,var}
+install -dv -m 0750 /root
+install -dv -m 1777 /tmp /var/tmp
+mkdir -pv /usr/{,local/}{bin,include,lib,sbin,src}
+mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
+mkdir -v  /usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -v  /usr/libexec
+mkdir -pv /usr/{,local/}share/man/man{1..8}
+mkdir -v  /usr/lib/pkgconfig
+
+case $(uname -m) in
+    x86_64) mkdir -v /lib64 ;;
+esac
+mkdir -v /var/{log,mail,spool}
+ln -sv /run /var/run
+ln -sv /run/lock /var/lock
+mkdir -pv /var/{opt,cache,lib/{color,misc,locate},local}
+
+ln -sv /tools/bin/{bash,cat,chmod,dd,echo,ln,mkdir,pwd,rm,stty,touch} /bin
+ln -sv /tools/bin/{env,install,perl,printf}         /usr/bin
+ln -sv /tools/lib/libgcc_s.so{,.1}                  /usr/lib
+ln -sv /tools/lib/libstdc++.{a,so{,.6}}             /usr/lib
+ln -sv bash /bin/sh
+
+ln -sv /proc/self/mounts /etc/mtab
---
+++ profile
@@ -0,0 +1,5 @@
+# Begin /etc/profile
+
+export LANG=en_GB.iso88591
+
+# End /etc/profile
